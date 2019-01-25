59dc6b98072580809e2f6925379e3dab0c5f2000
arvin <arvinlorenz@gmail.com>
2019-01-25 19:01:18 +0800

    i think final

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
+app.use('/sounds', express.static(path.join(__dirname,'sounds')));
 app.use('/', express.static(path.join(__dirname,'dist/pos')));
 app.listen(process.env.PORT || 5000)
