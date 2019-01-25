4e79f19017504432d1290c7a62a14853f3ce3dc1
arvin <arvinlorenz@gmail.com>
2019-01-25 18:45:54 +0800

    edit again

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const app = express()
+const app = express();
+const path = require('path');
 
 app.use('/', express.static(path.join(__dirname,'dist/pos')));
 app.listen(process.env.PORT || 5000)
