69ec155baab1bb2a3f0931314a86890c0706ab8a
arvin <arvinlorenz@gmail.com>
2019-01-31 13:03:52 +0800

    fixed sku app but still not good because of token loading

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const path = require('path');
 
 app.use('/sounds', express.static(path.join(__dirname,'sounds')));
 app.use('/', express.static(path.join(__dirname,'dist/pos')));
+
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'dist/pos', index.html))
+}) //return to index if not about routes
+
+
 app.listen(process.env.PORT || 5000)
