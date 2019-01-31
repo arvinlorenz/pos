1b41957847ddabefc16ece9d2567fa31776f9391
arvin <arvinlorenz@gmail.com>
2019-01-31 15:48:45 +0800

    tried *

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 app.use('/sounds', express.static(path.join(__dirname,'sounds')));
 app.use('/', express.static(path.join(__dirname,'dist/pos')));
 
-app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'dist/pos', index.html))
-}) //return to index if not about routes
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'dist/pos', 'index.html'));
+});
 
 
 app.listen(process.env.PORT || 5000)
