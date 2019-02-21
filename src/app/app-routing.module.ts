2569eb57e17094098dbf2fd66f0b1008bdb319ca
arvin <arvinlorenz@gmail.com>
2019-02-21 16:59:16 +0800

    added chinese texts fixed login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,8 +24,8 @@ const routes: Routes = [
     children: [
       { path: '', component: InventoryWithSuppliersComponent },
       { path: ':itemId', component: InventoryWithSuppliersComponent },
-    ]
-  }, canActivate: [AuthGuard]
+    ], canActivate: [AuthGuard]
+  },
   { path: 'login', component: LoginComponent},
     
 ]
